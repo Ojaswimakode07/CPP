7b5aff21fa0a64e4bf9abb95b9e579627fd5a9b7
Ojaswi Makode <162621630+Ojaswimakode07@users.noreply.github.com>
2024-04-11 22:29:20 +0530

    Add files via upload

---
+++ HACKERANK/CLASSES/Class_when_i-o_is_automatically_operated.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+struct student
+{
+   int age,standard;
+   char first_name[50],last_name[50];
+    
+};
+int main()
+{
+    student st;
+    //when i/o is automatically operated
+    cin>>st.age>>st.standard>>st.first_name>>st.last_name;
+    cout<<st.age<<" "<<st.standard<<" "<<st.first_name<<" "<<st.last_name;
+    return 0;
+}
