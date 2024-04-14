956ac99085c6a15bb905aef6ecc1eb3b465e8213
Ojaswi Makode <162621630+Ojaswimakode07@users.noreply.github.com>
2024-04-14 15:06:07 +0530

    Update Class_when_i-o_is_automatically_operated.cpp

--- HACKERANK/CLASSES/Class_when_i-o_is_automatically_operated.cpp
+++ HACKERANK/CLASSES/Class_when_i-o_is_automatically_operated.cpp
@@ -2,12 +2,14 @@
 using namespace std;
 struct student
 {
+ //private declaration
    int age,standard;
    char first_name[50],last_name[50];
     
 };
 int main()
 {
+   //main function declaration is done after the class declaration
     student st;
     //when i/o is automatically operated
     cin>>st.age>>st.standard>>st.first_name>>st.last_name;
